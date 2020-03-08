c20f2bfd75e9999c5fd015741c95e0f498a3d304
justin <jdemars@tophat.hsd1.ca.comcast.net>
2020-03-07 19:17:27 -0800

    added script changes.

--- python/sets/symmetric_diff.py
+++ python/sets/symmetric_diff.py
@@ -1,2 +1,10 @@
 n1 = int(input())
-l1 = map
+list1 = set(map(int, input(input if input.isdigit() else '')))
+n2 = int(input())
+list2 = set(map(int, input()))
+
+m = list1.difference(list2)
+n = list2.difference(list1)
+m = sorted(m.update(n))
+for i in m:
+    print(i)
